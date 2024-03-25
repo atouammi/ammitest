c24e3efafc0e0934f0da4ff182576a4a35d20cae
atouammi <akougbanhoun@aimsammi.org>
2024-03-25 23:33:40 +0000

    edit readme and code correction

--- NeuralNet/NeuralNet.py
+++ NeuralNet/NeuralNet.py
@@ -1,8 +1,8 @@
+
 import numpy as np
 
 import matplotlib.pyplot as plt
 
-np.random.seed(42)
 
 
 
@@ -22,6 +22,7 @@ X.shape, Y.shape
 
 
 
+
 # shuffle the data
 rand_perm = np.random.permutation(n)
 
@@ -30,113 +31,107 @@ Y = Y[rand_perm, :]
 
 
 
+
+X = X.T
+Y = Y.T
+X.shape, Y.shape
+
+
+
 # train test split
 ratio = 0.8
-X_train = X [:int (n*ratio)]
-Y_train = Y [:int (n*ratio)]
+X_train = X [:, :int (n*ratio)]
+Y_train = Y [:, :int (n*ratio)]
 
-X_test = X [:int (n*ratio)]
-Y_test = Y [:int (n*ratio)]
+X_test = X [:, int (n*ratio):]
+Y_test = Y [:, int (n*ratio):]
 
 
-
-plt.scatter(X_train[:,0], X_train[:,1], c=Y_train)
-plt.show()
-
+plt.scatter(X_train[0,:], X_train[1,:], c=Y_train[0,:])
+#plt.show()
 
 
 
 
 
-class Layer_Dense:
-    def __init__(self,n_inputs,n_neurons):
-        # initialization with xavier's technic
-        self.weights = np.random.normal(0,2/(n_inputs+n_neurons),(n_inputs,n_neurons))
-        
-        self.biases = np.ones((1,n_neurons))
-        
 
+def forward_pass(X, W1,W2, b1, b2):
+  Z1 = W1.dot(X) + b1
+  A1 = 1/(1+np.exp(-Z1))
+  Z2 = W2.dot(A1) + b2
+  A2 = 1/(1+np.exp(-Z2))
+  return A2, Z2, A1, Z1
+
+
+
+
+
+
+
+def backward_pass(X,Y, A2, Z2, A1, Z1, W1, W2, b1, b2):
     
-    #  the forward
-    def forward(self,X):
-        
-        self.output = np.dot(X , self.weights)+ self.biases
-# Activation function     
-class Activation_ReLu:
-    def forward(self,x):
-        self.output = np.maximum(0,x)
+  # Your code here
+  dl_dz2 = (A2-Y)/Y.shape[1]
+
+
+  dl_dz1 = (W2.T)@dl_dz2*(A1*(1-A1))
 
 
 
-class Activation_Sigmoid:
-    def forward(self,x):
-        self.output = 1/(1+np.exp(-x))
 
-class Activation_softmax:
+  dW1 =  dl_dz1@X.T
+
+
+  dW2 =  dl_dz2@A1.T
+
+
+  db1 = dl_dz1@np.ones(Y.shape).T
+
+  db2 = (dl_dz2)@np.ones(Y.shape).T
+
+  return dW1, dW2, db1, db2
+
+
+
+def loss(y_pred, Y):
     
-    def forward(self,x):
-        exp_values = np.exp(x-np.max(x, axis = 1, keepdims = True))
-        probabilites  =  exp_values / np.sum(exp_values,axis = 1, keepdims = True)
-        self.output = probabilites
+  # Your code here
 
-def LCE(y_pred,y_true):
-    
-    
-    # loss = -np.sum(y_true*np.log(y_pred)+(1-y_true)*np.log(1-y_pred))
-    loss = -np.mean(y_true*np.log(y_pred)+(1-y_true)*np.log(1-y_pred))
-    
-    return loss
-
-Layer1 = Layer_Dense(n_inputs=2, n_neurons=7)
-Activation1 = Activation_Sigmoid()
+  return  -np.mean(Y*np.log(y_pred)+(1-Y)*np.log(1-y_pred))
 
 
+def init_params():
+  
+  h0, h1, h2 = 2, 10, 1
 
+  W1 = 0.5*np.random.randn(h1,h0)
 
-Layer2 = Layer_Dense(n_inputs=7, n_neurons=1)
-Activation2 = Activation_Sigmoid()
+  W2 = 0.5*np.random.randn(h2,h1)
+
+  b1 = np.zeros((h1,1))
+
+  b2 = np.zeros((1,1))
+
+  return W1, W2, b1, b2
 
 
 
- # The back propagation
-def backward_pass(X,Y, A2,Z2, A1,Z1 ,W1, W2, b1, b2):
-
-
-
-  dl_dA2 = -((Y-A2)/(A2*(1-A2)))/Y.shape[0]
-
-
-  dA2_d_Z2 = (A2*(1-A2))
-
-
-  # dZ2_dA1 = W2.T
-
-  dA1_dZ1 = A1*(1-A1)
-
-  dW1 = X_train.T@((dl_dA2*dA2_d_Z2)@W2.T*dA1_dZ1)
-
-
-  dW2 = ( dl_dA2 * dA2_d_Z2 ).T @ A1
-
-  # one = np.ones((Z1.shape))
-  db1 = ((dl_dA2*dA2_d_Z2).T@dA1_dZ1)*W2.T
-
-  db2 = ( dl_dA2 .T@ dA2_d_Z2 )
-
-
-
-
-  return dW1 , dW2 , db1 , db2 
-
-
-
-
-# dW1 , dW2 , db1 , db2 = backward_pass(X_train,Y_train, A2, Z2, A1, Z1, W1, W2, b1, b2)
+def plot_decision_boundary(W1, W2, b1, b2):
+    x = np.linspace(-0.5, 2.5,100 )
+    y = np.linspace(-0.5, 2.5,100 )
+    xv , yv = np.meshgrid(x,y)
+    xv.shape , yv.shape
+    X_ = np.stack([xv,yv],axis = 0)
+    X_ = X_.reshape(2,-1)
+    A2, Z2, A1, Z1 = forward_pass(X_, W1, W2, b1, b2)
+    plt.figure()
+    plt.scatter(X_[0,:], X_[1,:], c= A2)
+    plt.show()
 
 
 
 def update(W1, W2, b1, b2,dW1, dW2, db1, db2, alpha ):
-
+    
   # Your code here
   W1 = W1-alpha*dW1
   W2 = W2 -alpha*dW2
@@ -151,69 +146,75 @@ def update(W1, W2, b1, b2,dW1, dW2, db1, db2, alpha ):
 
 
 
-alpha = 5*10e-5
-W1, W2, b1, b2 = Layer1.weights, Layer2.weights , Layer1.biases , Layer2.biases
-n_epochs = 10000
-
-train_loss = []
-for i in range(n_epochs):
-    
-    #predict
-    Layer1.forward(X_train)
-    Z1 = Layer1.output
-    Activation1.forward(Z1)
-    A1 = Activation1.output
 
 
-    Layer2.forward(A1)
-    Z2 = Layer2.output
-    Activation2.forward(Z2)
-    A2 = Activation2.output
+
+class Model: 
+   def __init__(self):
+         self.train_loss = []
+         self.test_loss = []
+         self.W1 = None
+         self.W2 = None
+         self.b1 = None
+         self.b2 = None
     
-    
-    
-    y_pred = A2
-    
-    
-    Loss = LCE(y_pred,Y_train)
-    train_loss.append(Loss)
-    
-    if i%10 == 0:
-        print(Loss)
+ 
+
+   def fit(self,X_train, Y_train,alpha = 0.1,n_epochs = 10000):
         
+
+        self.W1, self.W2, self.b1, self.b2 = init_params()
+        for i in range(n_epochs):
+            ## forward pass
+            A2, Z2, A1, Z1 =  forward_pass(X_train, self.W1,self.W2, self.b1, self.b2)
+            ## backward pass
+            dW1, dW2, db1, db2 = backward_pass(X_train,Y_train, A2, Z2, A1, Z1, self.W1, self.W2, self.b1, self.b2)
+            ## update parameters
+            self.W1, self.W2, self.b1, self.b2 = update(self.W1, self.W2, self.b1, self.b2,dW1, dW2, db1, db2, alpha )
+
+            ## save the train loss
+            self.train_loss.append(loss(A2, Y_train))
+            ## compute test loss
+            A2, Z2, A1, Z1 = forward_pass(X_test, self.W1, self.W2, self.b1, self.b2)
+            self.test_loss.append(loss(A2, Y_test))
+
+            ## plot boundary
+            if i %1000 == 0:
+                plot_decision_boundary(self.W1, self.W2, self.b1, self.b2)
+
+
+
+
+
+
+   def predict(self,X):
+        A2 = forward_pass(X, self.W1,self.W2, self.b1, self.b2)[0]
+        return [1 if s > 0.5 else 0 for s in A2[0]]
         
-    dW1 , dW2 , db1 , db2 = backward_pass(X_train,Y_train, A2, A1,Z2,Z1, W1, W2, b1, b2)
-    
-    W1, W2, b1, b2  = update(W1, W2, b1, b2,dW1, dW2, db1, db2, alpha )
-    Layer1.weights, Layer2.weights , Layer1.biases , Layer2.biases  = W1, W2, b1, b2
-plt.plot(train_loss)
+   def accuracy(self,y_pred, y):
+        return np.mean(y_pred ==y)
+
+    ## plot train et test losses
+   
+
+classifier = Model()
+
+
+classifier.fit(X_train,Y_train)
+
+plt.plot(classifier.train_loss)
+plt.plot(classifier.test_loss)
 plt.show()
 
 
 
 
-def predict(X_train):
-    Layer1.forward(X_train)
-    Z1 = Layer1.output
-    Activation1.forward(Z1)
-    A1 = Activation1.output
 
 
-    Layer2.forward(A1)
-    Z2 = Layer2.output
-    Activation2.forward(Z2)
-    A2 = Activation2.output
-    
-    
-    
-    return [1 if s > 0.5   else 0 for s in A2 ]
+y_pred = classifier.predict(X_train)
+train_accuracy = classifier.accuracy(y_pred, Y_train)
+print ("train accuracy :", train_accuracy)
 
-
-y_pred = predict(X_test)
-
-def accuracy(y_pred, y_true):
-    
-    return np.mean(y_pred==y_true)
-
-
-print("accuracy=",accuracy(y_pred, Y_test))
+y_pred = classifier.predict(X_test)
+test_accuracy = classifier.accuracy(y_pred, Y_test)
+print ("test accuracy :", test_accuracy)
--- NeuralNet/README.md
+++ NeuralNet/README.md
@@ -1,20 +1,43 @@
 # BootCamp AMMI 2024- Neural Network
 
 
-## This repositories contains some implementation from scratch of Neural network
+####  This repository contains code for a neural network implemented from scratch using `Python` and `NumPy`. The neural network architecture consists of 2 input nodes, `1 hidden layer` with `10 units`, and `1 output node`.
 
-
+## OVERVIEW
 ### This code has :
-- A class `Layer Dense` tha I use to after to create my layers
-- A class Activation of activation functions `Sigmoid`, ` ReLu` ,`Soltmax`, but I use only  the `Sigmoid`
-- Script for the training , using Gradient descent as Optimizer and `NLL` as a loss function
+- A class `Model` tha I use to after to create my layers.
+  The class `Model` has the attributes `W1` and `W2` as weigths of the neural network and biases `b1` , `b2`.
 
-- Some functions: `prediction()` and , `Accuracy`
+  $X---W_1---b_1--->Z_1--->A_1----W_2---b_2----Z_2--->A_2$
+
+  $ Z_1 = W_1X $, 
+  
+  $A1 = \sigma(Z_1) $, 
+  
+  $Z_2 = W_2A_1 $,
+
+  $A_2 = \sigma(Z_2)$ where $ \sigma(z) = \dfrac{1}{1+e^{-z}}$ is the activation function called `sigmoid`.
+
+
+
+  The class `Model` has also some methods like `fit` , `predit`, `accurracy`.
+
+
+-  Gradient Descent is the optimizer used
+
+- The loss function used is the `Negative log-Likehood`
 
 
 
 
 
+## This is  the plot  the train and the test.
+![alt text](<figure losses.png>) ![alt text]
+
+
+## The Decision Boundary of the result.
+
+![alt text](<Decision boundary-1.png>)
 # License
 [MIT](https://choosealicense.com/licenses/mit/)
 
